81112dcd7c9d1d91b250fd2d43b7bcbda21b424e
nartco <sebastien.thuillier@hotmail.com>
2019-03-11 16:39:37 +0100

    first

---
+++ TP_JS/script.js
@@ -0,0 +1,7 @@
+var chatmessage = [];
+
+function message() {
+    var id = 0;
+    var pseudo = prompt('veuillez saisir votre pseudo');
+    var message = prompt('veuillez saisir votre message')
+}
